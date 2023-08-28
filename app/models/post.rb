21c6d2312d296ddc4d4635688838cbc79ddb3065
johannkrugell <johannkrugell@gmail.com>
2023-08-29 06:14:15 +1000

    ADD UNIQUENESS VALIDATION TO POST MODEL Add uniqueness validation to post model to prevent duplicate posts. The title of the post must be unique.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app/models/post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   has_many :comments
   # Validation
   # A post must have a title
-  validates :title, presence: true, length: { maximum: 20 }
+  validates :title, presence: true, length: { maximum: 20 }, uniqueness: true
   # A post must have a body
   validates :body, presence: true, length: { maximum: 100 }
 end
